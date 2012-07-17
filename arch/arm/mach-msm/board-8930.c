59f0b9dbc2b46c21b37180a739eff260a3012511
Jayasena Sangaraboina <jsanga@codeaurora.org>
2012-07-17 12:03:08 -0700

    ASoc: msm: Add low latency playback and recording support.
    - Add lowlatency pcm driver for Playback and Recording.
    - Add support in target board files
    - Add Recording Path to Multimedia5 FE DAI
    - Add support in routing, platform, machine drivers
    - Add low latency interfaces support in ASM and ADM drivers.
    
    Change-Id: I1beb11db9010534e5aa91179ac6040a41622185d
    Signed-off-by: Jayasena Sangaraboina <jsanga@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/board-8930.c
    	sound/soc/msm/qdsp6/q6asm.c

--- arch/arm/mach-msm/board-8930.c
+++ arch/arm/mach-msm/board-8930.c
@@ -2318,6 +2318,7 @@ static struct platform_device *cdp_devices[] __initdata = {
 	&msm_cpudai_incall_record_rx,
 	&msm_cpudai_incall_record_tx,
 	&msm_pcm_hostless,
+	&msm_lowlatency_pcm,
 };
 
 static void __init msm8930_i2c_init(void)
